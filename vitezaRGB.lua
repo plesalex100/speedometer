4efcbe872216f035ae8d4b9ac7ba18898776427d
Mihnea Banditul <47335726+MihneaBanditul@users.noreply.github.com>
2019-03-06 00:25:48 +0200

    Add files via upload

---
+++ vitezaRGB.lua
@@ -0,0 +1,39 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1)
+        if(IsPedInAnyVehicle(GetPlayerPed(-1), false))then
+			local speed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false)) * 3.6
+          --  Draw2Txt(1.407, 1.30, 1.14,1.0,0.45,"Viteza", 255,255,255,255)
+          drawTxt(1.407, 1.30, 1.07,1.0,0.55,"" .. math.ceil(speed) .." KM/H" , 255, 255, 255, 255)
+        --  drawTxt(1.407, 1.30, 1.04,1.0,0.55, " KMH", 255, 255, 255, 255)
+            drawTxt(1.407, 1.30, 1.14,1.0,0.55, " Viteza", 255, 255, 255, 255)
+        end
+        
+	end
+    end)
+
+    local function RGBRainbow( frequency )
+        local result = {}
+        local curtime = GetGameTimer() / 1000
+    
+        result.r = math.floor( math.sin( curtime * frequency + 0 ) * 127 + 128 )
+        result.g = math.floor( math.sin( curtime * frequency + 2 ) * 127 + 128 )
+        result.b = math.floor( math.sin( curtime * frequency + 4 ) * 127 + 128 )
+    
+        return result
+    end
+
+
+    function drawTxt(x,y ,width,height,scale, text, r,g,b,a)
+        SetTextFont(4)
+        SetTextProportional(0)
+        SetTextScale(scale, scale)
+        local rainbow = RGBRainbow( 1 )
+        SetTextColour( rainbow.r, rainbow.g, rainbow.b, 255 )
+        SetTextDropShadow(0, 0, 0, 0,255)
+        SetTextEdge(1, 0, 0, 0, 255)
+        SetTextOutline()
+        SetTextEntry("STRING")
+        AddTextComponentString(text)
+        DrawText(x - width/2, y - height/2 + 0.005)
+    end 
